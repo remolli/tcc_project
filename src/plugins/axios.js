9845b51ef56799b7f4c6a5b395965cf44f9b3a72
Evandro <evandro_remolli.t@outlook.com>
2024-06-30 00:38:16 -0300

    :sparkles: feat: axios requests configurated

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -9,7 +9,7 @@ import axios from "axios";
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-  // baseURL: process.env.baseURL || process.env.apiUrl || ""
+  baseURL: process.env.VUE_APP_BASE_URL
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
@@ -18,6 +18,9 @@ const _axios = axios.create(config);
 
 _axios.interceptors.request.use(
   function(config) {
+    config.params = {
+      'api-key': process.env.VUE_APP_TOKEN_API
+    }
     // Do something before request is sent
     return config;
   },
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="home">
-    
+    <b-col>
+      <b-row v-for="(item,idx) in items" :key="idx" class="m-0" align-h="center">
+        {{ item.title }}
+      </b-row>
+    </b-col>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomeView',
+  data(){
+    return {
+      items: []
+    }
+  },
+  async created(){
+    await this.loadData();
+  },
+  methods: {
+    async loadData(){
+      const response = await this.$axios.get('mostpopular/v2/emailed/7.json');
+      console.log(response);
+      this.items = response.data.results;
+    },
+  }
 }
 </script>

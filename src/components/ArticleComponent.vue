476d94b5c7a7a3ada620007d217bbe46700e28a6
Evandro <evandro_remolli.t@outlook.com>
2024-08-16 14:24:23 -0300

    :lipstick: feat: image exhibition

--- src/components/ArticleComponent.vue
+++ src/components/ArticleComponent.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="px-5 py-4 my-3" style="border:1px solid #E1E1E3; background-color:#FAFAFC; border-radius:8px; text-align:start;">
         <b-col>
+            <b-row v-for="(item,idx) in media" :key="idx" 
+            class="d-flex justify-content-end align-items-center" style="flex-direction:row-reverse;"> 
+                <img v-for="(image,idx) in item['media-metadata']" :key="idx" 
+                :src="image.url" 
+                :style="{
+                    'max-width': image.width+'px',
+                    'max-height': image.height+'px',
+                    'border-radius': '20px',
+                    'object-fit': 'contain',
+                }">
+            </b-row>
             <b-row> Title:  {{ title || '---' }}  </b-row>
             <b-row> Abstract:  {{ abstract || '---' }}  </b-row>
             <b-row> Section:  {{ section || '---' }}  </b-row>
             <b-row> Subsection:  {{ subsection || '---' }}  </b-row>
             <b-row> {{ writtenBy || '---' }}  </b-row>
-            <!-- <b-row> Media:  {{ media || '---' }}  </b-row> -->
             <b-row> Published at:  {{ publishDate || '---' }}  </b-row>
             <b-row> Last updated at:  {{ lastEditDate || '---' }}  </b-row>
             <b-row> Url:  {{ url || '---' }}  </b-row>
